223c299776364fb287f15a34c49035f2c8ceb503
Hanyoung Cho <rumidier@gmail.com>
2011-11-18 15:57:11 +0900

    Check get, post
    get, post형식을 실험 하였고
    user_add, group_add 함수를 만들것이다

--- samba/mywebapp/lib/mywebapp.pm
+++ samba/mywebapp/lib/mywebapp.pm
@@ -13,28 +13,25 @@ get '/' => sub {
 };
 
 get '/select' => sub {
-    template 'select', {
-        u_add => '/add',
-        u_del => '/u_del',
-    };
+    template 'select';
+};
+
+get '/add' => sub {
+	template 'add';
 };
 
 post '/add' => sub {
-	template 'add', {
-		action => '/u_add',
-	};
-};
-
-post '/u_add' => sub {
     my $u_id     = param('u_id');
     my $u_name   = param('u_name');
     my $u_passwd = param('passwd');
     my $d_group  = param('d_group');
     my $a_group  = param('a_group');
 
-    user_add ($u_id, $pu->encpass($u_passwd), $pu->maxuid + 1);
+    mkdir "/home/s-rumidier/$u_id";
+=pod
+    user_add ($u_id, $u_passwd, $u_name)
     group_add ($d_group, $a_group);
-
+=cut
     template 'add', {
         u_id     => $u_id,
         u_name   => $u_name,
@@ -42,8 +39,10 @@ post '/u_add' => sub {
         d_group  => $d_group,
         a_group  => $a_group,
     };
+
 };
 
+=pod
 sub user_add {
     my $pu = Passwd::Unix->new();
 
@@ -54,7 +53,6 @@ $u_name, "/home/s-rumidier/$u_id", "/sbin/nologin");
 
 sub group_add {
 };
-=pod
 
 post '/sum' => sub {
     "I say a number:".params->{number};
