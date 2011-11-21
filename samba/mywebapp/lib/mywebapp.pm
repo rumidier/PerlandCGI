64edbf76972a7bcc2b7e12b52f7e57eaf38ee181
Hanyoung Cho <rumidier@gmail.com>
2011-11-21 14:09:24 +0900

    Testing checkbox

--- samba/mywebapp/lib/mywebapp.pm
+++ samba/mywebapp/lib/mywebapp.pm
@@ -2,8 +2,9 @@ package mywebapp;
 
 use 5.014;
 use Passwd::Unix;
-use Template;
 use Unix::GroupFile;
+use Template;
+use Data::Dumper;
 use Dancer ':syntax';
 
 our $VERSION = '0.1';
@@ -26,12 +27,23 @@ post '/add' => sub {
     my $u_passwd = param('passwd');
     my $d_group  = param('d_group');
     my $a_group  = param('a_group');
+    my $groups   = param('l_group');
 
-    mkdir "/home/s-rumidier/$u_id";
-=pod
-    user_add ($u_id, $u_passwd, $u_name)
-    group_add ($d_group, $a_group);
-=cut
+#    my @group_s = $groups; 
+    debug "\n";
+    debug "\n";
+    debug "\n";
+    debug "\n";
+    debug "$groups";
+    debug "\n";
+    debug "\n";
+    debug "\n";
+    debug "\n";
+    debug "\n";
+
+#    user_add ($u_id, $u_passwd, $u_name);
+#    group_add ($u_id, $a_group);
+
     template 'add', {
         u_id     => $u_id,
         u_name   => $u_name,
@@ -43,15 +55,37 @@ post '/add' => sub {
 };
 
 =pod
+sub mk_dir {
+    my $id = shift;
+    mkdir "/home/s-rumidier/$id";
+};
+
 sub user_add {
+    my ($id, $passwd, $name) = @_;
     my $pu = Passwd::Unix->new();
 
-    my $err = $pu->user($u_id, $pu->encpass($u_passwd), $pu->maxuid + 1, $pu->maxuid + 1,
-$u_name, "/home/s-rumidier/$u_id", "/sbin/nologin"); 
-    mkdir "/home/s-rumidier/$u_id";
+    my $err = $pu->user(
+		    $id,
+		    $pu->encpass($passwd),
+		    $pu->maxuid + 1,
+		    $pu->maxuid + 1,
+		    $name,
+		    "/home/s-rumidier/$id",
+		    "/sbin/nologin"
+		    ); 
+
+    mk_dir($id);
 };
 
 sub group_add {
+    my $id = shift;
+    my @group = @_;
+    my $grp = new Unix::GroupFile "/etc/group";
+    $grp->add_usr("cv", $id);
+
+    for ( 0 .. $#group ) {
+        $grp->add_usr($group[$_], $id);
+    }
 };
 
 post '/sum' => sub {
