90c62da2b3c70c1f35212fcf9988d152b47fcaba
Hanyoung Cho <rumidier@gmail.com>
2011-11-29 17:38:31 +0900

    add, del modified

--- samba/mywebapp/lib/mywebapp.pm
+++ samba/mywebapp/lib/mywebapp.pm
@@ -4,10 +4,12 @@ use 5.014;
 use warnings;
 use strict;
 use Passwd::Unix;
+use File::Path qw/ make_path remove_tree /;
 use Unix::GroupFile;
 use Template;
 use Data::Dumper;
 use Dancer ':syntax';
+use Encode 'from_to';
 
 our $VERSION = '0.1';
 
@@ -33,14 +35,7 @@ post '/add' => sub {
 # groups가 NULL 인지 비교한후 NULL이 아니면 문자열 인지, arry ref 인지
 # 확인하여 처리한다. 
 #
-    if (defined $groups) {
-        if (ref($groups) eq 'ARRAY') {
-            debug ref($groups);
-        }
-        elsif (!ref($groups)) {
-            debug "----------  DEBUG -------------";
-        }
-    }
+    user_add($u_id, $u_passwd, $u_name, $groups);
 
     template 'add', {
         u_id     => $u_id,
@@ -57,36 +52,75 @@ get '/del' => sub {
 post '/del' => sub {
     my $u_id = param('u_id');
 
-    user_del($u_id);
-
 	template 'del';
+    user_del($u_id);
 };
 
 sub mk_dir {
     my $id = shift;
-    mkdir "/home/s-rumidier/$id";
+
+    my $dir = "/home/$id";
+    make_path ($dir,
+            {
+                owner   => "$id",
+                group   => 'cv',
+                mode    => 0700
+            } );
+};
+
+sub rm_dir {
+    my $id = shift;
+    rmtree ("/home/$id",
+            {
+                owner   => '$id',
+                group   => 'cv',
+                verbose => 1,
+                mode    => 0755
+            } );
 };
 
 sub user_add {
-    my ($id, $passwd, $name) = @_;
+    my ($id, $passwd, $name, $groups) = @_;
     my $pu = Passwd::Unix->new();
+    my $grp = new Unix::GroupFile "/etc/group";
 
     my $err = $pu->user(
 		    $id,
 		    $pu->encpass($passwd),
 		    $pu->maxuid + 1,
-		    $pu->maxuid + 1,
+		    $grp->gid('cv'),
 		    $name,
-		    "/home/s-rumidier/$id",
+		    "/home/$id",
 		    "/sbin/nologin"
 		    ); 
 
-    mk_dir($id);
+    if (defined $groups) {
+        if (ref($groups) eq 'ARRAY') {
+            my $group_count = @{ $groups } - 1;
+
+            for (0 .. $group_count) {
+                $grp->add_user("$groups->[$_]", $id);
+            }
+        }
+        elsif (!ref($groups)) {
+            $grp->add_user($groups, $id);
+        }
+
+        $grp->commit();
+    }
+
+ mk_dir($id);
 };
 
 sub user_del {
     my @users = qw/ 
                     rumidier-test
+                    rumidier-test1
+                    rumidier-test2
+                    rumidier-test3
+                    rumidier-test4
+                    rumidier-test5
+                    rumidier-test6
                     rumidier-test-16
                     rmidier-test-16-2
                     /;
@@ -101,43 +135,5 @@ sub user_del {
     }
 
 };
-
-=pod
-sub group_add {
-    my $id = shift;
-    my @group = @_;
-    my $grp = new Unix::GroupFile "/etc/group";
-    $grp->add_usr("cv", $id);
-
-    for ( 0 .. $#group ) {
-        $grp->add_usr($group[$_], $id);
-    }
-};
-
-post '/sum' => sub {
-    "I say a number:".params->{number};
-};
-
-get '/add' => sub {
-    template 'test', {
-        action => '/add',
-        num1   => 0,
-        num2   => 0,
-        sum    => 0,
-    };
-};
-
-post '/add' => sub {
-    my $num1 = param('num-1') || 0;
-    my $num2 = param('num-2') || 0;
-    template 'test', {
-        action  => '/add',
-        num1    => $num1,
-        num2    => $num2,
-        sum     => $num1 + $num2,
-    };
-};
-=cut
-
 
 true;
